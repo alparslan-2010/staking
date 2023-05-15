bf03fe35a9190d6e9b1d966c0721769225ed93ba
alparslan-2010 <alparslannihatt@gmail.com>
2023-05-15 22:45:10 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { ConnectWallet } from "@thirdweb-dev/react";
+import { useContract, useContractRead } from "@thirdweb-dev/react";
+import { Contract } from 'ethers';
+import { useAddress } from "@thirdweb-dev/react";
+import { ethers } from 'ethers';
+
+
 
 function App() {
+
+  const  {contract} = useContract("0x41f7e79E6E8a631e04361e0F42b9Dd5a3D930e7D");
+  console.log(contract);
+  const address = useAddress();
+
+ 
+  const { data, isLoading } = useContractRead(contract, "getStakeInfo", [address])
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <ConnectWallet />
+    
+   Staked : {data?._tokenStaked &&ethers.utils.formatEther(data?._rewards) } <br></br>
+   rewards :{data?._rewards &&ethers.utils.formatEther(data?._rewards)}   
+     
+    </>
   );
 }
 
